97df8ba8e2f1e127a1047283644ba713325ed72d
aliraza-addweb <aliraza.k.addweb@gmail.com>
2021-07-30 12:44:15 +0530

    28 july task

---
+++ 28-07-2021/index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+
+var app = express();
+app.listen(5013);
+
+console.log("Something awesome to happen at http://localhost:5013");
+
+
+app.get("/", function (request, response){
+response.sendFile(__dirname+"/textbox.html");
+});
+app.get("/getsum", function (request, response){
+var num1 = parseInt(request.query.Num1);
+var num2 = parseInt(request.query.Num2);
+
+var sum = num1 + num2;
+response.send("sum:"+sum)
+
+});
+
