ae221b96970de7d325d6485dcee87d54e94a1a02
aliraza-addweb <aliraza.k.addweb@gmail.com>
2021-07-30 11:47:20 +0530

    29 july task

---
+++ 29-07-2021/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3044;
+
+const staticpath = path.join(__dirname, "../public");
+console.log(staticpath);
+
+app.use(express.static(staticpath));
+
+app.listen(port,() => {
+console.log('listening the port');
+});
