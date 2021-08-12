59e2f9b30141d143d010f075bb13f3e741857fcd
aliraza-addweb <aliraza.k.addweb@gmail.com>
2021-08-12 13:01:28 +0530

    3 August task

---
+++ 3-8-2021/input.js
@@ -0,0 +1,119 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
+const path = require('path')
+const { check, validationResult } = require('express-validator')
+
+const app = express()
+const port = 5005
+
+// Set Templating Enginge
+app.set('view engine', 'ejs')
+
+app.use(
+    fileUpload({
+      useTempFiles: true,
+      tempFileDir: path.join(__dirname, 'tmp'),
+      createParentPath: true,
+      limits: { fileSize: 2 * 1024 * 1024 },
+    })
+  )
+
+
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(bodyParser.json());
+
+// Navigation
+app.get('', (req, res)=> {
+    res.render('index')
+})
+
+app.get('/register', (req, res)=> {
+    res.render('register')
+})
+
+app.post('/register', urlencodedParser, [
+    check('username')
+        .isAlpha().withMessage('Name must be alphabetic.')
+        .notEmpty()
+        .exists()
+        .isLength({ min: 3 }).withMessage('This username must me 3+ characters long'),
+    check('phonenumber')
+        .isNumeric()
+        .isLength({min:10,max:10})
+        .withMessage('Phone number is not valid'),
+    check('email')
+        .isEmail()
+        .normalizeEmail()
+        .withMessage('Email is not valid'),
+    check('dob')
+        .isDate()
+        .withMessage('Must be a valid date')
+        .custom((dob, { req }) => {
+            const [sd, sm, sy] = dob.split('-')
+            console.log(sd);
+            console.log(sm);
+            console.log(sy);
+            if(sd > 2003){
+                throw new Error(
+                    'Date should before 2003')
+                }
+                else{
+                    return true
+                }
+            
+        })
+            ,
+    check('password')
+        .exists()
+        .isLength({min: 4,max: 14})
+        .withMessage('Password must between 4-14 charactor')
+
+], (req, res)=> {
+
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        // return res.status(422).jsonp(errors.array())
+        const alert = errors.array()
+        res.render('register', {
+            alert
+        })
+    }
+    else{
+
+        var data1 = req.body.username;
+        var data2 = req.body.phonenumber;
+        var data3 = req.body.email;
+        var dob = req.body.dob;
+        var data4 = req.body.password;
+        var data5 = req.body.textarea;
+        var data6 = req.files.myfile;
+        console.log(data6);
+
+    
+            
+            //console.log(file)
+            // const fileName = new Date().getTime().toString() + path.extname(file.name)
+            const savePath = path.join(__dirname, 'public', 'uploads',data6.name)
+            // if (file.truncated) {
+            //   throw new Error('File size is too big...')
+            // }
+            // if (file.mimetype !== 'image/jpeg') {
+            //   throw new Error('Only jpegs are supported')
+            // }
+            // console.log('gfgfgfg')
+            data6.mv(savePath)
+           // res.redirect('/')
+          
+        }
+
+        console.log(dob);
+        res.json({name: data1, phonenumber: data2,email: data3,dob: dob,password: data4,textarea: data5})
+    }
+
+
+)
+
+
+app.listen(port, () => console.info(`App listening on port: ${port}`))
